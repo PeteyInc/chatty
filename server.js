4577309e05c7c098aa8bdfeba850db6c1e68dac3
Jared M. Peterson <jared@peteyinc.com>
2016-06-08 16:17:50 -0600

    server up

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var messages = [];
+var messages = ["hello"];
 var app = express();
 var port = 9999;
 
-
+app.use(bodyParser.json());
 app.listen(9999);
+
+
+app.get('/', function(req, res, next) {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).json(messages);
+});
+
+app.post('/', function(req, res, next) {
+    messages.push(req.body.message);
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).json(messages);
+});
